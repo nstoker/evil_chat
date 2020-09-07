2270f5b275420c93329a26c77835a4e80f903339
Neil Stoker <neil.stoker@gmail.com>
2020-09-07 15:02:28 +0100

    Correct typos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def component(component_name, locals = {}, &block)
     name = component_name.split("_").first
-    render("components/#{name}#{component_name}", locals &block)
+    render("components/#{name}/#{component_name}", locals, &block)
   end
 
   alias c component
